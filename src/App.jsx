9eb38038ed695ceb69e2e79ea7c141fa41e58497
AshutoshDM1 <downlodemaster1@gmail.com>
2024-04-30 21:34:16 +0530

    landing page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./component/navbar";
+import Body from "./component/landing_body";
 
 function App() {
   return (
     <>
       <div className="landing_Page">
         <Navbar />
+        <Body />
+        
       </div>
     </>
   );
---
+++ src/component/landing_body.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style/landing_body.css";
+const Body = () => {
+  return (
+    <>
+      <div id="container">
+        <div className="main">
+          <h6>- Track Market</h6>
+          <h2>Receive sms notification</h2>
+          <h2>when prices hit target</h2>
+          <h2>Levels</h2>
+        </div>
+        <div id="body_img">
+          <img src="/body_text-removebg-preview.png" />
+        </div>
+        <div className="main">
+          <h3>Keep track of the lastest prices and trends in the crypotocurrency</h3>
+          <h3>market</h3>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Body;
